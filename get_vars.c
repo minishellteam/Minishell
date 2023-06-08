7ea081bf6c676c407dfb3e21da4af448f53d30be
Mairym15 <111610715+Mairym15@users.noreply.github.com>
2023-06-08 13:51:19 +0200

    vars replaced by variables functional, norm friendly 0 leaks

--- get_vars.c
+++ get_vars.c
@@ -6,7 +6,7 @@
 /*   By: mkerkeni <mkerkeni@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 16:16:52 by mkerkeni          #+#    #+#             */
-/*   Updated: 2023/06/08 10:36:31 by mkerkeni         ###   ########.fr       */
+/*   Updated: 2023/06/08 13:50:03 by mkerkeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,25 @@ static char	*replace_var_by_value(char *line, char *value, int start, int end)
 {
 	char	*before_dollar;
 	char	*after_var;
-
-	before_dollar = ft_substr(line, 0, start - 1);
+	char	*tmp;
+	int		x;
+	
+	x = 1;
+	if (!value)
+		x = 2;
+	before_dollar = ft_substr(line, 0, start - x);
 	after_var = ft_substr(line, end, ft_strlen(line) - end);
+	free(line);
 	if (!value)
 		line = ft_strjoin(before_dollar, after_var);
 	else
 	{
-		line = ft_strjoin(before_dollar, value);
-		line = ft_strjoin(line, after_var);
+		tmp = ft_strjoin(before_dollar, value);
+		line = ft_strjoin(tmp, after_var);
+		free(tmp);
 	}
+	free(before_dollar);
+	free(after_var);
 	return (line);
 }
 
@@ -45,9 +54,8 @@ static char	*handle_var(char *new_line, char *var, char *value)
 				i++;
 			var = ft_substr(new_line, j, i - j);
 			value = getenv(var);
+			free(var);
 			new_line = replace_var_by_value(new_line, value, j, i);
-			var = NULL;
-			value = NULL;
 			i = 0;
 		}
 		else
@@ -64,6 +72,7 @@ char	*get_value_vars(t_data cmd)
 
 	var = NULL;
 	value = NULL;
+	new_line = NULL;
 	new_line = ft_strdup(cmd.line);
 	new_line = handle_var(new_line, var, value);
 	printf("newline = %s\n", new_line);
