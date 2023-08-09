c7d339b79c14af5b97d655fd11a3f41cd6d67708
mkerkeni <m.gharbi-rouis@hotmail.fr>
2023-08-10 01:02:53 +0200

    parsing almost over, just need to handle here doc, global variable deleted

--- parser/get_string_type.c
+++ parser/get_string_type.c
@@ -6,7 +6,7 @@
 /*   By: mkerkeni <mkerkeni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 11:27:08 by mkerkeni          #+#    #+#             */
-/*   Updated: 2023/08/09 01:03:36 by mkerkeni         ###   ########.fr       */
+/*   Updated: 2023/08/10 00:56:49 by mkerkeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,12 @@ void	get_files(t_tok *toks)
 	while (tmp && tmp->next)
 	{
 		if (is_chevron(tmp->type, 1))
-			tmp->next->type = "FILE";
+		{
+			if (!ft_strncmp(tmp->type, "DOUBLE_LESSER", ft_strlen(tmp->type)))
+				tmp->next->type = "DELIMITER";
+			else
+				tmp->next->type = "FILE";
+		}
 		tmp = tmp->next;
 	}
 }
